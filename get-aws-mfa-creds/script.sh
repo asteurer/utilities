e0fd8a5a3866d846ac15ed7d6e542c60cc72654c
Andrew Steurer <andrew.steurer@fermyon.com>
2024-06-20 21:11:02 -0600

    refactoring

--- get-aws-mfa-creds/script.sh
+++ get-aws-mfa-creds/script.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-op run --env-file=.env -- go run main.go
+op run \
+    --env-file=/home/andyboi/Repositories/System/utilities/get-aws-mfa-creds/.env \
+    -- go run /home/andyboi/Repositories/System/utilities/get-aws-mfa-creds/main.go
