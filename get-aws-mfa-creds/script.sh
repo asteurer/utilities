bac352f65ed436ea8e841dfb66863a3108b68d82
Andrew Steurer <andrew.steurer@fermyon.com>
2024-06-25 23:55:37 -0600

    Adding functionality for placing temp creds in 1Password

--- get-aws-mfa-creds/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-op run \
-    --env-file=/home/andyboi/Repositories/System/utilities/get-aws-mfa-creds/.env \
-    -- go run /home/andyboi/Repositories/System/utilities/get-aws-mfa-creds/main.go
